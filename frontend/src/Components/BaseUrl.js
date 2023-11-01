2892cbacb3635b1937cd5e5d0fd0df73ba1bd91f
Tanvir Mahmud <tanvirmahmudfahim1313@gmail.com>
2023-11-01 20:39:11 +0600

    remove build folder

--- frontend/src/Components/BaseUrl.js
+++ frontend/src/Components/BaseUrl.js
@@ -1,4 +1,4 @@
 export default {
-    // baseUrl: 'http://127.0.0.1:8000',
-    baseUrl: 'https://tanvir66.pythonanywhere.com'
+    baseUrl: 'http://127.0.0.1:8000',
+    // baseUrl: 'https://tanvir66.pythonanywhere.com'
 };
